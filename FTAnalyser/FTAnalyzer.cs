5d51ee0031bf88ce19c19f3a64e8ddc956c29a8a
Levva_cp <SND\Levva_cp>
2010-02-16 09:55:12 +0000

    Rename Project files to Analyzer

--- FTAnalyser/FTAnalyzer.cs
+++ FTAnalyser/FTAnalyzer.cs
@@ -4,7 +4,7 @@ using System.Windows.Forms;
 
 namespace FTAnalyser
 {
-    static class FTAnalyser
+    static class FTAnalyzer
     {
         /// <summary>
         /// The main entry point for the application.
