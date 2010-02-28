d00734d79b08d23734f946730abd9abf463686bc
Levva_cp <SND\Levva_cp>
2010-02-28 22:11:19 +0000

    Add DummyMain

---
+++ Old Java Code/DummyMain.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FTAnalyser
+{
+    static class DummyMain
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+         }
+    }
+}
