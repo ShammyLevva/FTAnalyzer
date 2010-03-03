6fd2905dc9d58cd0305695dd06be231a51811011
Levva_cp <SND\Levva_cp>
2010-03-03 11:30:58 +0000

    Added RegistrationsProcessor

---
+++ FTAnalyser/Registrations/RegistrationsProcessor.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace FTAnalyser
+{
+    public class RegistrationsProcessor
+    {
+
+        RegistrationFilter filter;
+        IComparer<Registration> comparator;
+
+        public RegistrationsProcessor(RegistrationFilter f, IComparer<Registration> c)
+        {
+            filter = f;
+            comparator = c;
+        }
+
+        public RegistrationsProcessor(RegistrationFilter f) 
+            : this(f, null)
+        { }
+
+        public RegistrationsProcessor(Comparator<Registration> c)
+            : this(new AllFilter(), c)
+        { }
+
+        public RegistrationsProcessor()
+            : this(new AllFilter(), null)
+        { }
+
+        private List<Registration> filterRegistrations(List<Registration> regs) {
+            List<Registration> result = new List<Registration>(regs.Count);
+            foreach (Registration r in regs) {
+                if (filter.select(r))
+                    result.Add(r);
+            }
+            return result;
+        }
+
+        private List<Registration> sortRegistrations(List<Registration> regs)
+        {
+            if (comparator != null)
+                regs.Sort(comparator);
+            return regs;
+        }
+
+        public List<Registration> processRegistrations(List<Registration> regs)
+        {
+            return sortRegistrations(filterRegistrations(regs));
+        }
+    }
+}
