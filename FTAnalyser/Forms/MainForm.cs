96a964ef8a5b34a72b78afb1da488ac09e4daf52
Levva_cp <SND\Levva_cp>
2010-03-11 11:34:07 +0000

    Bump version to v1.0.2

--- FTAnalyser/Forms/MainForm.cs
+++ FTAnalyser/Forms/MainForm.cs
@@ -12,7 +12,7 @@ namespace FTAnalyzer
 {
     public partial class MainForm : Form
     {
-        private string VERSION = "1.0.1";
+        private string VERSION = "1.0.2";
 
         private Cursor storedCursor = Cursors.Default;
         private FamilyTree ft = FamilyTree.Instance;
