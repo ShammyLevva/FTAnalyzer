476d38dd3cc5ec6107a75894726f9b33c62b5151
Levva_cp <SND\Levva_cp>
2010-03-11 12:28:34 +0000

    Add version display on main form titlebar

--- FTAnalyser/Forms/MainForm.cs
+++ FTAnalyser/Forms/MainForm.cs
@@ -371,6 +371,7 @@ namespace FTAnalyzer
         {
             _timerCheckForUpdates = new System.Threading.Timer(new System.Threading.TimerCallback(_timerCheckForUpdates_Callback));
             _timerCheckForUpdates.Change(3000, 1000 * 60 * 60 * 8); //Check for updates 3 sec after the form loads, and then again every 8 hours
+            this.Text = "Family Tree Analyzer v" + VERSION;
         }
 
         private void checkForUpdatesToolStripMenuItem_Click(object sender, EventArgs e)
