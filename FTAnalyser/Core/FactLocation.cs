a4ea9526ef04e66adf667a1a59d346bb7201ff19
Levva_cp <SND\Levva_cp>
2010-03-17 11:07:39 +0000

    Added fixes for UK regions/shires/counties where country is missing

--- FTAnalyser/Core/FactLocation.cs
+++ FTAnalyser/Core/FactLocation.cs
@@ -11,7 +11,8 @@ namespace FTAnalyzer
 	    public static readonly string SCOTLAND = "Scotland", ENGLAND = "England", CANADA = "Canada", USA = "United States",
             WALES = "Wales", IRELAND = "Ireland", UK = "UK";
 	    public static readonly string ABERDEEN = "Aberdeenshire", AYR = "Ayrshire", KINCARDINE = "Kincardineshire",
-			    LANARK = "Lanarkshire", BANFF = "Banffshire", ANGUS = "Angus", MIDLOTHIAN = "Midlothian", FIFE = "Fife";
+			    LANARK = "Lanarkshire", BANFF = "Banffshire", ANGUS = "Angus", MIDLOTHIAN = "Midlothian", FIFE = "Fife",
+                MIDDLESEX = "Middlesex";
     	
         public const int COUNTRY = 0, REGION = 1, PARISH = 2, ADDRESS = 3, PLACE = 4;
 
@@ -25,6 +26,134 @@ namespace FTAnalyzer
         private int level;
 
         private List<Individual> individuals;
+        private static Dictionary<string, string> CITYSHIFTS = new Dictionary<string,string>();
+        private static Dictionary<string, string> COUNTRYFIXES = new Dictionary<string, string>();
+        private static Dictionary<string, string> COUNTRYSHIFTS = new Dictionary<string, string>();
+
+        static FactLocation() {
+            CITYSHIFTS.Add("Aberdeen", ABERDEEN);
+            CITYSHIFTS.Add("Ayr", AYR);
+            CITYSHIFTS.Add("Glasgow", LANARK);
+            CITYSHIFTS.Add("Dundee", ANGUS);
+            CITYSHIFTS.Add("Edinburgh", MIDLOTHIAN);
+            CITYSHIFTS.Add("London", MIDDLESEX);
+
+            COUNTRYFIXES.Add("US", USA);
+            COUNTRYFIXES.Add("USA", USA);
+            
+            // Scottish Regions
+            COUNTRYSHIFTS.Add("Aberdeenshire", SCOTLAND);
+            COUNTRYSHIFTS.Add("Angus", SCOTLAND);
+            COUNTRYSHIFTS.Add("Argyll", SCOTLAND);
+            COUNTRYSHIFTS.Add("Argyllshire", SCOTLAND);
+            COUNTRYSHIFTS.Add("Ayrshire", SCOTLAND);
+            COUNTRYSHIFTS.Add("Banff", SCOTLAND);
+            COUNTRYSHIFTS.Add("Banffshire", SCOTLAND);
+            COUNTRYSHIFTS.Add("Berwick", SCOTLAND);
+            COUNTRYSHIFTS.Add("Berwickshire", SCOTLAND);
+            COUNTRYSHIFTS.Add("Bute", SCOTLAND);
+            COUNTRYSHIFTS.Add("Bute", SCOTLAND);
+            COUNTRYSHIFTS.Add("Caithness", SCOTLAND);
+            COUNTRYSHIFTS.Add("Clackmannanshire", SCOTLAND);
+            COUNTRYSHIFTS.Add("Dumfries", SCOTLAND);
+            COUNTRYSHIFTS.Add("Dumfriesshire", SCOTLAND);
+            COUNTRYSHIFTS.Add("Dumfries-shire", SCOTLAND);
+            COUNTRYSHIFTS.Add("Dumbarton", SCOTLAND);
+            COUNTRYSHIFTS.Add("Dumbartonshire", SCOTLAND);
+            COUNTRYSHIFTS.Add("East Lothian", SCOTLAND);
+            COUNTRYSHIFTS.Add("Fife", SCOTLAND);
+            COUNTRYSHIFTS.Add("Fifeshire", SCOTLAND);
+            COUNTRYSHIFTS.Add("Inverness", SCOTLAND);
+            COUNTRYSHIFTS.Add("Inverness-shire", SCOTLAND);
+            COUNTRYSHIFTS.Add("Kincardineshire", SCOTLAND);
+            COUNTRYSHIFTS.Add("Kinross", SCOTLAND);
+            COUNTRYSHIFTS.Add("Kinross-shire", SCOTLAND);
+            COUNTRYSHIFTS.Add("Kirkcudbright", SCOTLAND);
+            COUNTRYSHIFTS.Add("Kirkcudbrightshire", SCOTLAND);
+            COUNTRYSHIFTS.Add("Lanarkshire", SCOTLAND);
+            COUNTRYSHIFTS.Add("Midlothian", SCOTLAND);
+            COUNTRYSHIFTS.Add("Moray", SCOTLAND);
+            COUNTRYSHIFTS.Add("Moaryshire", SCOTLAND);
+            COUNTRYSHIFTS.Add("Nairn", SCOTLAND);
+            COUNTRYSHIFTS.Add("Nairnshire", SCOTLAND);
+            COUNTRYSHIFTS.Add("Orkney", SCOTLAND);
+            COUNTRYSHIFTS.Add("Peebles-shire", SCOTLAND);
+            COUNTRYSHIFTS.Add("Perthshire", SCOTLAND);
+            COUNTRYSHIFTS.Add("Renfrewshire", SCOTLAND);
+            COUNTRYSHIFTS.Add("Ross & Cromarty", SCOTLAND);
+            COUNTRYSHIFTS.Add("Ross and Cromarty", SCOTLAND);
+            COUNTRYSHIFTS.Add("Roxburgh", SCOTLAND);
+            COUNTRYSHIFTS.Add("Selkirk", SCOTLAND);
+            COUNTRYSHIFTS.Add("Shetland", SCOTLAND);
+            COUNTRYSHIFTS.Add("Zetland", SCOTLAND);
+            COUNTRYSHIFTS.Add("Stirlingshire", SCOTLAND);
+            COUNTRYSHIFTS.Add("Sutherland", SCOTLAND);
+            COUNTRYSHIFTS.Add("West Lothian", SCOTLAND);
+            COUNTRYSHIFTS.Add("Wigtonshire", SCOTLAND);
+            
+            // English Shires & Counties
+            COUNTRYSHIFTS.Add("Bedfordshire", ENGLAND);
+            COUNTRYSHIFTS.Add("Berkshire", ENGLAND);
+            COUNTRYSHIFTS.Add("Buckinghamshire", ENGLAND);
+            COUNTRYSHIFTS.Add("Cambridgeshire", ENGLAND);
+            COUNTRYSHIFTS.Add("Cheshire", ENGLAND);
+            COUNTRYSHIFTS.Add("Cornwall", ENGLAND);
+            COUNTRYSHIFTS.Add("Cumberland", ENGLAND);
+            COUNTRYSHIFTS.Add("Cumbria", ENGLAND);
+            COUNTRYSHIFTS.Add("Derbyshire", ENGLAND);
+            COUNTRYSHIFTS.Add("Dorset", ENGLAND);
+            COUNTRYSHIFTS.Add("Devon", ENGLAND);
+            COUNTRYSHIFTS.Add("Co.Durham", ENGLAND);
+            COUNTRYSHIFTS.Add("Durham", ENGLAND);
+            COUNTRYSHIFTS.Add("Essex", ENGLAND);
+            COUNTRYSHIFTS.Add("Gloucestershire", ENGLAND);
+            COUNTRYSHIFTS.Add("Hampshire", ENGLAND);
+            COUNTRYSHIFTS.Add("Herefordshire", ENGLAND);
+            COUNTRYSHIFTS.Add("Hertfordshire", ENGLAND);
+            COUNTRYSHIFTS.Add("Huntingdonshire", ENGLAND);
+            COUNTRYSHIFTS.Add("Kent", ENGLAND);
+            COUNTRYSHIFTS.Add("Lancashire", ENGLAND);
+            COUNTRYSHIFTS.Add("Leicestershire", ENGLAND);
+            COUNTRYSHIFTS.Add("Lincolnshire", ENGLAND);
+            COUNTRYSHIFTS.Add("Monmouthshire", ENGLAND);
+            COUNTRYSHIFTS.Add("Norfolk", ENGLAND);
+            COUNTRYSHIFTS.Add("Northamptonshire", ENGLAND);
+            COUNTRYSHIFTS.Add("Northumberland", ENGLAND);
+            COUNTRYSHIFTS.Add("Nottinghamshire", ENGLAND);
+            COUNTRYSHIFTS.Add("Oxfordshire", ENGLAND);
+            COUNTRYSHIFTS.Add("Rutland", ENGLAND);
+            COUNTRYSHIFTS.Add("Shropshire", ENGLAND);
+            COUNTRYSHIFTS.Add("Somerset", ENGLAND);
+            COUNTRYSHIFTS.Add("Staffordshire", ENGLAND);
+            COUNTRYSHIFTS.Add("Suffolk", ENGLAND);
+            COUNTRYSHIFTS.Add("Sussex", ENGLAND);
+            COUNTRYSHIFTS.Add("Surrey", ENGLAND);
+            COUNTRYSHIFTS.Add("Warwickshire", ENGLAND);
+            COUNTRYSHIFTS.Add("Westmorland", ENGLAND);
+            COUNTRYSHIFTS.Add("Westmorlandshire", ENGLAND);
+            COUNTRYSHIFTS.Add("Wiltshire", ENGLAND);
+            COUNTRYSHIFTS.Add("Worcestershire", ENGLAND);
+            COUNTRYSHIFTS.Add("Yorkshire", ENGLAND);
+            COUNTRYSHIFTS.Add("North Yorkshire", ENGLAND);
+            COUNTRYSHIFTS.Add("West Yorkshire", ENGLAND);
+            COUNTRYSHIFTS.Add("South Yorkshire", ENGLAND);
+
+            // Welsh Regions
+            COUNTRYSHIFTS.Add("Anglesey", WALES);
+            COUNTRYSHIFTS.Add("Breconshire", WALES);
+            COUNTRYSHIFTS.Add("Caernarvon", WALES);
+            COUNTRYSHIFTS.Add("Cardiganshire", WALES);
+            COUNTRYSHIFTS.Add("Carmarthenshire", WALES);
+            COUNTRYSHIFTS.Add("Denbighshire", WALES);
+            COUNTRYSHIFTS.Add("Glamorganshire", WALES);
+            COUNTRYSHIFTS.Add("Gwynedd", WALES);
+            COUNTRYSHIFTS.Add("Merionethshire", WALES);
+            COUNTRYSHIFTS.Add("Mid-Glamorgan", WALES);
+            COUNTRYSHIFTS.Add("Montgomeryshire", WALES);
+            COUNTRYSHIFTS.Add("Pembrokeshire", WALES);
+            COUNTRYSHIFTS.Add("Radnorshire", WALES);
+            
+        }
         
         public FactLocation() {
             this.location = "";
@@ -75,21 +204,24 @@ namespace FTAnalyzer
 	                level = COUNTRY;
 	            }
 	            fixCities();
+                fixCountries();
             }
+        }
+
+        private void fixCountries()
+        {
+            string newCountry = string.Empty;
+            COUNTRYSHIFTS.TryGetValue(country, out newCountry);
+            if (newCountry != null && newCountry.Length > 0)
+                shiftCountry(newCountry);
         }
 
         private void fixCities()
         {
-            if (region.Equals("Aberdeen"))
-                shiftRegion(ABERDEEN);
-            else if (region.Equals("Ayr"))
-                shiftRegion(AYR);
-            else if (region.Equals("Glasgow"))
-        	    shiftRegion(LANARK);
-            else if (region.Equals("Dundee"))
-        	    shiftRegion(ANGUS);
-            else if (region.Equals("Edinburgh"))
-        	    shiftRegion(MIDLOTHIAN);
+            string newRegion = string.Empty;
+            CITYSHIFTS.TryGetValue(region, out newRegion);
+            if (newRegion != null && newRegion.Length > 0)
+                shiftRegion(newRegion);
         }
         
         private void shiftRegion(string newRegion) {
@@ -97,6 +229,16 @@ namespace FTAnalyzer
             address = parish;
             parish = region;
             region = newRegion;
+            if (level < PLACE) level++; // we have moved up a level
+        }
+
+        private void shiftCountry(string newCountry)
+        {
+            place = (place + " " + address).Trim();
+            address = parish;
+            parish = region;
+            region = country;
+            country = newCountry;
             if (level < PLACE) level++; // we have moved up a level
         }
 
