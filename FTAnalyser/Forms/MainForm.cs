266f0e7b8fc22a8343bac4b10119d2dd242c6533
Levva_cp <SND\Levva_cp>
2013-08-11 11:40:03 +0000

    bump to v1.6.0.0

--- FTAnalyser/Forms/MainForm.cs
+++ FTAnalyser/Forms/MainForm.cs
@@ -17,7 +17,7 @@ namespace FTAnalyzer
 {
     public partial class MainForm : Form
     {
-        private string VERSION = "1.6.0.0-Tim Test 1";
+        private string VERSION = "1.6.0.0";
         private bool _checkForUpdatesEnabled = true;
         private bool _showNoUpdateMessage = false;
         private System.Threading.Timer _timerCheckForUpdates;
