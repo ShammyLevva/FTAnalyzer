2b8d6c691a93a8fd12ab16427c48b0b6b5b16606
Levva_cp <SND\Levva_cp>
2013-08-17 17:56:36 +0000

    add more locations

--- FTAnalyser/Core/FactLocation.cs
+++ FTAnalyser/Core/FactLocation.cs
@@ -13,7 +13,8 @@ namespace FTAnalyzer
 		
 		public static readonly string SCOTLAND = "Scotland", ENGLAND = "England", CANADA = "Canada", UNITED_STATES = "United States",
 			WALES = "Wales", IRELAND = "Ireland", UNITED_KINGDOM = "United Kingdom", NEW_ZEALAND = "New Zealand", AUSTRALIA = "Australia", 
-            UNKNOWN_COUNTRY = "Unknown", ENG_WALES = "England & Wales", INDIA = "India", FRANCE = "France", GERMANY = "Germany";
+            UNKNOWN_COUNTRY = "Unknown", ENG_WALES = "England & Wales", INDIA = "India", FRANCE = "France", GERMANY = "Germany", 
+            ITALY = "Italy", SPAIN = "Spain";
 		
 		public const int UNKNOWN = -1, COUNTRY = 0, REGION = 1, PARISH = 2, ADDRESS = 3, PLACE = 4;
 
@@ -204,10 +205,10 @@ namespace FTAnalyzer
             if (country.Equals(ENGLAND) || country.Equals(SCOTLAND) || country.Equals(WALES) || country.Equals(IRELAND) || 
                 country.Equals(UNITED_STATES) || country.Equals(UNITED_KINGDOM) || country.Equals(CANADA) ||
                 country.Equals(NEW_ZEALAND) || country.Equals(AUSTRALIA) || country.Equals(INDIA) || country.Equals(GERMANY) ||
-                country.Equals(FRANCE))
+                country.Equals(FRANCE) || country.Equals(SPAIN) || country.Equals(ITALY))
                 knownCountry = true;
         }
-
+    
 		#region Fix Location string routines
 		private void fixEmptyFields()
 		{
