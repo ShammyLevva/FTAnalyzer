139e48bebf719bb27356aa6c31c76b761bd09a36
Levva_cp <SND\Levva_cp>
2013-08-24 00:24:41 +0000

    Published as version 2.1.2.0

--- FTAnalyser/Forms/MainForm.cs
+++ FTAnalyser/Forms/MainForm.cs
@@ -19,7 +19,7 @@ namespace FTAnalyzer
 {
     public partial class MainForm : Form
     {
-        private string VERSION = "2.1.1.1";
+        private string VERSION = "2.1.2.0";
         //private bool _checkForUpdatesEnabled = false;
         //private bool _showNoUpdateMessage = false;
         //private System.Threading.Timer _timerCheckForUpdates;
