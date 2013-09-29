a94d38a28acf49949e81ae387ad3f80c9066a63b
Levva_cp <SND\Levva_cp>
2013-09-29 15:53:59 +0000

    Bump version to 2.3.0.0

--- FTAnalyser/Forms/MainForm.cs
+++ FTAnalyser/Forms/MainForm.cs
@@ -19,7 +19,7 @@ namespace FTAnalyzer
 {
     public partial class MainForm : Form
     {
-        private string VERSION = "2.2.1.2";
+        private string VERSION = "2.3.0.0";
         //private bool _checkForUpdatesEnabled = false;
         //private bool _showNoUpdateMessage = false;
         //private System.Threading.Timer _timerCheckForUpdates;
