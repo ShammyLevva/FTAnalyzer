85d246be1b40474e142f015973abe61da86561e1
Levva_cp <SND\Levva_cp>
2013-10-04 14:08:44 +0000

    Add MarkerClusterer

---
+++ FTAnalyser/Utilities/MarkerClusterer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using GoogleMapsApi;
+
+namespace FTAnalyzer.Utilities
+{
+    public class MarkerClusterer
+    {
+    }
+}
