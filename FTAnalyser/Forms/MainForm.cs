044a216ab49a9fe79c6eac00f8c2ff403a62c963
Levva_cp <SND\Levva_cp>
2013-11-02 11:19:33 +0000

    Publish as v3.0.3.0

--- FTAnalyser/Forms/MainForm.cs
+++ FTAnalyser/Forms/MainForm.cs
@@ -22,7 +22,7 @@ namespace FTAnalyzer
 {
     public partial class MainForm : Form
     {
-        private string VERSION = "3.0.3.0-beta-test4";
+        private string VERSION = "3.0.3.0";
 
         private Cursor storedCursor = Cursors.Default;
         private FamilyTree ft = FamilyTree.Instance;
