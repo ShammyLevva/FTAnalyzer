fdbe14c5c1acac56d876e4b5be2fe12b2aecf4ae
Levva_cp <SND\Levva_cp>
2013-11-09 18:28:38 +0000

    Added Parental Relationship Class need to replace FamiliesAsChild with new class

---
+++ FTAnalyser/Core/ParentalRelationship.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FTAnalyzer.Core
+{
+    public class ParentalRelationship
+    {
+        public enum ParentalRelationshipType { NATURAL, ADOPTED, STEP, FOSTER, RELATED, GUARDIAN, SEALED, PRIVATE, UNKNOWN };
+
+        public Family Family { get; private set; }
+        public ParentalRelationshipType FatherRelationship { get; private set; }
+        public ParentalRelationshipType MotherRelationship { get; private set; }
+
+        public ParentalRelationship(Family family, ParentalRelationshipType father, ParentalRelationshipType mother)
+        {
+            this.Family = family;
+            this.FatherRelationship = father;
+            this.MotherRelationship = mother;
+        }
+    }
+}
