cabc26621f2800b5f3f1aad4eb10726d7dbecb19
Levva_cp <SND\Levva_cp>
2013-11-28 17:28:44 +0000

    Tweak location stats counts as status of empty location differs?

--- FTAnalyser/Core/FamilyTree.cs
+++ FTAnalyser/Core/FamilyTree.cs
@@ -1943,8 +1943,8 @@ namespace FTAnalyzer
             if (geocoding)
                 xmlErrorbox.AppendText("\nGeocoding results:");
             // write geocode results - ignore UNKNOWN entry
-            int notsearched = (FactLocation.AllLocations.Count(x => x.GeocodeStatus.Equals(FactLocation.Geocode.NOT_SEARCHED)) - 1);
-            xmlErrorbox.AppendText("\nFound " + (FactLocation.AllLocations.Count() - 1) + " locations in file.\n");
+            int notsearched = FactLocation.AllLocations.Count(x => x.GeocodeStatus.Equals(FactLocation.Geocode.NOT_SEARCHED));
+            xmlErrorbox.AppendText("\nFound " + FactLocation.AllLocations.Count() + " locations in file.\n");
             xmlErrorbox.AppendText("    " + FactLocation.AllLocations.Count(x => x.GeocodeStatus.Equals(FactLocation.Geocode.GEDCOM_USER) && x.GoogleLocation.Length > 0) + " are GEDCOM/User Entered and have been geocoded.\n");
             xmlErrorbox.AppendText("    " + FactLocation.AllLocations.Count(x => x.GeocodeStatus.Equals(FactLocation.Geocode.GEDCOM_USER) && x.GoogleLocation.Length == 0) + " are GEDCOM/User Entered but lack a Google Location.\n");
             xmlErrorbox.AppendText("    " + FactLocation.AllLocations.Count(x => x.GeocodeStatus.Equals(FactLocation.Geocode.MATCHED)) + " have a geocoding match from Google.\n");
