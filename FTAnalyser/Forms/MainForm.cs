4aef06fec62c16637240ad68326262dc98f963af
Levva_cp <SND\Levva_cp>
2014-03-22 14:07:51 +0000

    Release as Beta 9

--- FTAnalyser/Forms/MainForm.cs
+++ FTAnalyser/Forms/MainForm.cs
@@ -21,7 +21,7 @@ namespace FTAnalyzer
 {
     public partial class MainForm : Form
     {
-        public string VERSION = "3.4.0.0-beta 8";
+        public string VERSION = "3.4.0.0-beta 9";
 
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         private Cursor storedCursor = Cursors.Default;
