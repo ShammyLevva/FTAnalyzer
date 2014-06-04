1d311cd6b6ed0d5dfce7dc44646780efc9641f87
Levva_cp <SND\Levva_cp>
2014-06-04 11:04:32 +0000

    Add classes for recognised region support

---
+++ FTAnalyser/Core/Region.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FTAnalyzer
+{
+    public class Region
+    {
+        public string RegionName { get; private set; }
+        public string Country { get; private set; }
+        public List<string> AlternativeNames { get; private set; }
+
+        public Region(string region, string country)
+        {
+            Country = country;
+            RegionName = region;
+            AlternativeNames = new List<string>();
+        }
+
+        public void AddAlternateName(string name)
+        {
+            AlternativeNames.Add(name);
+        }
+    }
+}
---
+++ FTAnalyser/Core/Regions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FTAnalyzer
+{
+    public class Regions
+    {
+        public static List<Region> SCOTTISH_REGIONS;
+        public static List<Region> ENGLISH_REGIONS;
+        public static List<Region> WELSH_REGIONS;
+        public static List<Region> UK_REGIONS;
+
+        static Regions()
+        {
+            SCOTTISH_REGIONS = new List<Region>();
+            ENGLISH_REGIONS = new List<Region>();
+            WELSH_REGIONS = new List<Region>();
+
+            UK_REGIONS = new List<Region>();
+            UK_REGIONS.AddRange(SCOTTISH_REGIONS);
+            UK_REGIONS.AddRange(ENGLISH_REGIONS);
+            UK_REGIONS.AddRange(WELSH_REGIONS);
+        }
+    }
+}
