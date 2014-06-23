b6f4736ff9e7dbc8fdaa47ec297bfc1a16352788
Levva_cp <SND\Levva_cp>
2014-06-23 09:01:03 +0000

    Add Children status family name for sorting

--- FTAnalyser/Core/Displays/IDisplayChildrenStatus.cs
+++ FTAnalyser/Core/Displays/IDisplayChildrenStatus.cs
@@ -8,6 +8,7 @@ namespace FTAnalyzer
     public interface IDisplayChildrenStatus
     {
         string FamilyID { get; }
+        string Surname { get; }
         string HusbandID { get; }
         string Husband { get; }
         string WifeID { get; }
