d9e16fa183f932ab9f4daa5292c2abc7e64d310a
Levva_cp <SND\Levva_cp>
2015-04-07 13:22:42 +0000

    quotSometextquot in Wiki strings now formatted correctly

--- FTAnalyser/Core/FamilyTree.cs
+++ FTAnalyser/Core/FamilyTree.cs
@@ -2703,9 +2703,13 @@ namespace FTAnalyzer
 
         private string FixWikiFormatting(string input)
         {
-            string result = input.Replace("ampampnbsp", " ").Replace("ampampndash", "-");
+            string result = input.Replace("ampampnbsp", " ").Replace("ampnbsp", " ").Replace("ampampndash", "-").Replace("ampndash", "-");
+            //strip out {{cite xxxxx }} citation text with its urls
             Regex rgx = new Regex("{{.*}}");
             result = rgx.Replace(result, string.Empty);
+            Match match = Regex.Match(result, "(.*)quot(.*)quot(.*)", RegexOptions.IgnoreCase);
+            if(match.Success)
+                result = match.Groups[1].ToString().Trim() + " " + '\u0022' + match.Groups[2].ToString().Trim() + '\u0022' + " " + match.Groups[3].ToString().Trim();
             return result;
         }
 
