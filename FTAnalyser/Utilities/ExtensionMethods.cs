1bbdf21c26405b0f2c7c2af3846b4aee862c5d94
Levva_cp <ShammyLevva@users.noreply.github.com>
2017-01-31 14:42:46 +0000

    Fix for lone text on a line after a CONT or CONC line. Updated to v5.2.0.0

--- FTAnalyser/Utilities/ExtensionMethods.cs
+++ FTAnalyser/Utilities/ExtensionMethods.cs
@@ -33,5 +33,12 @@ namespace FTAnalyzer.Utilities
             if (date < FactDate.MINDATE) date = FactDate.MINDATE;
             return date;
         }
+
+        public static Boolean StartsWithNumeric(this String input)
+        {
+            if (input.Length == 0) return false;
+            char first = input[0];
+            return (first == '0' || first == '1' || first == '2' || first == '3' || first == '4' || first == '5' || first == '6' || first == '7' || first == '8' || first == '9');
+        }
     }
 }
