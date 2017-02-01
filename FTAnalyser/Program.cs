23c2eb624580a9b86202d4eb7bf6dc3eaf87cdc5
Levva_cp <ShammyLevva@users.noreply.github.com>
2017-02-01 11:47:08 +0000

    Added debugging logging

--- FTAnalyser/Program.cs
+++ FTAnalyser/Program.cs
@@ -6,14 +6,19 @@ namespace FTAnalyzer
 {
     static class Program
     {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            log.Info("Reached Main");
             Application.EnableVisualStyles();
+            log.Info("--Set Visual Styles");
             Application.SetCompatibleTextRenderingDefault(false);
+            log.Info("--Set Compatible Text Rendering");
             Application.Run(new MainForm());
         }
     }
--- FTAnalyser/Properties/AssemblyInfo.cs
+++ FTAnalyser/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FTAnalyzer")]
-[assembly: AssemblyCopyright("Copyright © Alexander Bisset & Deryck Brown 2010-2013")]
+[assembly: AssemblyCopyright("Copyright © Alexander Bisset & Deryck Brown 2010-2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,3 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
