b119abaffc7d92f1ef74b782e896b265938dc228
Alexander Bisset <ambisset@btinternet.com>
2017-06-26 00:44:19 +0100

    apply code suggestions

--- FTAnalyser/GedcomParser/AnselEncoding.cs
+++ FTAnalyser/GedcomParser/AnselEncoding.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Text;
 
 namespace FTAnalyzer
 {
-    class AnselEncoding : System.Text.Encoding
+    class AnselEncoding : Encoding
     {
         public override int GetByteCount(char[] chars, int index, int count)
         {
