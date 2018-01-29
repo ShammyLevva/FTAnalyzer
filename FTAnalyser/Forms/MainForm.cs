df6bf3badbaf7adc9affa4cf20e76f94626fc4c5
Alexander Bisset <ambisset@btinternet.com>
2018-01-29 22:59:22 +0000

    Remove reference to SQLite 1.0.104 Bump to v6.1.0.2

--- FTAnalyser/Forms/MainForm.cs
+++ FTAnalyser/Forms/MainForm.cs
@@ -22,7 +22,7 @@ namespace FTAnalyzer
 {
     public partial class MainForm : Form
     {
-        public static string VERSION = "6.1.0.1";
+        public static string VERSION = "6.1.0.2";
 
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         
