d986dcc4e617624beb39adc5f042f9d002c8be1c
Alexander Bisset <ambisset@btinternet.com>
2018-02-05 20:57:19 +0000

    Remove SharpMap submodule now embedded in Solution

--- FTAnalyser/SharedAssemblyVersion.cs
+++ FTAnalyser/SharedAssemblyVersion.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Reflection.AssemblyInformationalVersion("1.2.0.6a7a89eb")]
+[assembly: System.Reflection.AssemblyInformationalVersion("1.2.0.480f5832")]
 [assembly: System.Reflection.AssemblyVersion("1.2.0")]
 [assembly: System.Reflection.AssemblyFileVersion("1.2.0")]
 
