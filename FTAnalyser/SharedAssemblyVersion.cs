03647bf1ac284d1d65df5061fc3c4169110802e4
Alexander Bisset <ambisset@btinternet.com>
2018-02-07 20:39:03 +0000

    remove unnecessary usings

--- FTAnalyser/SharedAssemblyVersion.cs
+++ FTAnalyser/SharedAssemblyVersion.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Reflection.AssemblyInformationalVersion("1.2.0.7f22e992")]
+[assembly: System.Reflection.AssemblyInformationalVersion("1.2.0.2ceae2f3")]
 [assembly: System.Reflection.AssemblyVersion("1.2.0")]
 [assembly: System.Reflection.AssemblyFileVersion("1.2.0")]
 
--- FTAnalyzer.Shared/Core/Displays/IDisplayFact.cs
+++ FTAnalyzer.Shared/Core/Displays/IDisplayFact.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿#if !__MAC__
 using System.Drawing;
+#endif
 
 namespace FTAnalyzer
 {
