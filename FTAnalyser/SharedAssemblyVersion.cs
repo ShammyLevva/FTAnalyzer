44df0a1f6786689680a90e48c65b540a7bea1d44
Alexander Bisset <ambisset@btinternet.com>
2018-03-13 12:47:30 +0000

    Rebuild all

--- FTAnalyser/SharedAssemblyVersion.cs
+++ FTAnalyser/SharedAssemblyVersion.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Reflection.AssemblyInformationalVersion("1.2.0.60095e99")]
+[assembly: System.Reflection.AssemblyInformationalVersion("1.2.0.07f83394")]
 [assembly: System.Reflection.AssemblyVersion("1.2.0")]
 [assembly: System.Reflection.AssemblyFileVersion("1.2.0")]
 
