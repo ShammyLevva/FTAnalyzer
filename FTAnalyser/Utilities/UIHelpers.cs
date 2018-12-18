be3a4813f75b814656fb3945adfd6f67422c9dbf
Alexander Bisset <A-Bisset@aberdeen-harbour.co.uk>
2018-12-18 13:41:44 +0000

    resinstate badline count warning

---
+++ FTAnalyser/Utilities/UIHelpers.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+
+namespace FTAnalyzer.Utilities
+{
+    public static class UIHelpers
+    {
+        public static bool ShowYesNo(string message)
+        {
+            DialogResult result = MessageBox.Show(message, "Continue Loading?", MessageBoxButtons.YesNo);
+            return result == DialogResult.Yes;
+         }
+    }
+}
