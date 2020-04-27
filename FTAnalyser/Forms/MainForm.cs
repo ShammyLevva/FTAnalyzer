346ca3aedf11a4b69962f7b4d508c4f65de362bc
Alexander Bisset <A-Bisset@aberdeen-harbour.co.uk>
2020-04-27 22:26:26 +0100

    Set version to beta3

--- FTAnalyser/Forms/MainForm.cs
+++ FTAnalyser/Forms/MainForm.cs
@@ -30,7 +30,7 @@ namespace FTAnalyzer
 {
     public partial class MainForm : Form
     {
-        public static string VERSION = "7.8.0.0-beta1";
+        public static string VERSION = "7.8.0.0-beta3";
 
         static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
