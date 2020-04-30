0fe76a522731f4dc6286def0840da25367fb17ff
Alexander Bisset <A-Bisset@aberdeen-harbour.co.uk>
2020-04-30 20:19:07 +0100

    Updated to analyzer v3 packages

--- FTAnalyser/Forms/MainForm.cs
+++ FTAnalyser/Forms/MainForm.cs
@@ -30,7 +30,7 @@ namespace FTAnalyzer
 {
     public partial class MainForm : Form
     {
-        public static string VERSION = "7.8.0.0-beta4";
+        public static string VERSION = "7.8.0.0-beta5";
 
         static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
