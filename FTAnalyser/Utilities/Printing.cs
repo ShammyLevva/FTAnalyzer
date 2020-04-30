af5c26a9c98b7ff364355af0874bedef63645027
Alexander Bisset <A-Bisset@aberdeen-harbour.co.uk>
2020-05-01 00:29:25 +0100

    tweak printing string reader before publish

--- FTAnalyser/Utilities/Printing.cs
+++ FTAnalyser/Utilities/Printing.cs
@@ -8,50 +8,38 @@ namespace FTAnalyzer.Utilities
     class Printing : IDisposable
     {
         readonly ScrollingRichTextBox rtb;
-        readonly StringReader reader;
         
         public Printing(ScrollingRichTextBox rtb)
         {
             this.rtb = rtb;
-            reader = new StringReader(rtb.Text);
         }
 
         public void PrintPage(object sender, PrintPageEventArgs e)
         {
             try
             {
-                float LeftMargin = e.MarginBounds.Left;
-                float TopMargin = e.MarginBounds.Top;
-                string Line = null;
-                Font PrintFont = rtb.Font;
-                if (PrintFont.SizeInPoints < 11)
-                    PrintFont = new Font(PrintFont.FontFamily, 11f);
-                int maxWidth = e.MarginBounds.Right - e.MarginBounds.Left;
-                int maxHeight = e.MarginBounds.Bottom - e.MarginBounds.Top;
-                float fontHeight = PrintFont.GetHeight(e.Graphics);
-                using (SolidBrush PrintBrush = new SolidBrush(Color.Black))
+                using (StringReader reader = new StringReader(rtb.Text))
                 {
-                    float YPosition = TopMargin;
-                    while (YPosition < maxHeight && ((Line = reader.ReadLine()) != null))
+                    float LeftMargin = e.MarginBounds.Left;
+                    float TopMargin = e.MarginBounds.Top;
+                    string Line = null;
+                    Font PrintFont = rtb.Font;
+                    if (PrintFont.SizeInPoints < 11)
+                        PrintFont = new Font(PrintFont.FontFamily, 11f);
+                    int maxWidth = e.MarginBounds.Right - e.MarginBounds.Left;
+                    int maxHeight = e.MarginBounds.Bottom - e.MarginBounds.Top;
+                    float fontHeight = PrintFont.GetHeight(e.Graphics);
+                    using (SolidBrush PrintBrush = new SolidBrush(Color.Black))
                     {
-                        SizeF sf = e.Graphics.MeasureString(Line, PrintFont, maxWidth);
-                        e.Graphics.DrawString(Line, PrintFont, PrintBrush, new RectangleF(new PointF(LeftMargin, YPosition), sf), StringFormat.GenericTypographic);
-                        YPosition += sf.Height;
+                        float YPosition = TopMargin;
+                        while (YPosition < maxHeight && ((Line = reader.ReadLine()) != null))
+                        {
+                            SizeF sf = e.Graphics.MeasureString(Line, PrintFont, maxWidth);
+                            e.Graphics.DrawString(Line, PrintFont, PrintBrush, new RectangleF(new PointF(LeftMargin, YPosition), sf), StringFormat.GenericTypographic);
+                            YPosition += sf.Height;
+                        }
+                        e.HasMorePages = Line != null;
                     }
-                    e.HasMorePages = Line != null;
-                }
-            }
-            catch (Exception) { }
-        }
-
-        protected virtual void Dispose(bool disposing)
-        {
-            try
-            {
-                if (disposing)
-                {
-                    reader.Dispose();
-                    rtb.Dispose();
                 }
             }
             catch (Exception) { }
@@ -59,7 +47,7 @@ namespace FTAnalyzer.Utilities
 
         public void Dispose()
         {
-            Dispose(true);
+            rtb.Dispose();
             GC.SuppressFinalize(this);
         }
     }
