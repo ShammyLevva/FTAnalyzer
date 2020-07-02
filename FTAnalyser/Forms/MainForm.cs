9ad76adacf58271f806175a3a1044ac8f6a98096
Alexander Bisset <A-Bisset@aberdeen-harbour.co.uk>
2020-07-02 12:45:40 +0100

    set as beta1

--- FTAnalyser/Forms/MainForm.cs
+++ FTAnalyser/Forms/MainForm.cs
@@ -29,7 +29,7 @@ namespace FTAnalyzer
 {
     public partial class MainForm : Form
     {
-        public static string VERSION = "7.8.4.0";
+        public static string VERSION = "7.8.4.0-beta1";
 
         static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
