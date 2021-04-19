edcf2279f732418c0b33379881f2687860d617f7
ShammyLevva <mega2019www@gmail.com>
2021-04-19 21:06:18 +0100

    Add gender to display duplicates

--- FTAnalyser/Forms/Controls/VirtualDGVDuplicates.cs
+++ FTAnalyser/Forms/Controls/VirtualDGVDuplicates.cs
@@ -2,34 +2,38 @@
 {
     class VirtualDGVDuplicates : VirtualDataGridView<IDisplayDuplicateIndividual>
     {
-        protected override object GetValueFor(IDisplayDuplicateIndividual ind, string propertyName)
+        protected override object GetValueFor(IDisplayDuplicateIndividual dup, string propertyName)
         {
             switch (propertyName)
             {
                 case nameof(IDisplayDuplicateIndividual.IndividualID):
-                    return ind.IndividualID;
+                    return dup.IndividualID;
                 case nameof(IDisplayDuplicateIndividual.Name):
-                    return ind.Name;
+                    return dup.Name;
                 case nameof(IDisplayDuplicateIndividual.Forenames):
-                    return ind.Forenames;
+                    return dup.Forenames;
                 case nameof(IDisplayDuplicateIndividual.Surname):
-                    return ind.Surname;
+                    return dup.Surname;
                 case nameof(IDisplayDuplicateIndividual.BirthDate):
-                    return ind.BirthDate;
+                    return dup.BirthDate;
                 case nameof(IDisplayDuplicateIndividual.BirthLocation):
-                    return ind.BirthLocation;
+                    return dup.BirthLocation;
+                case nameof(IDisplayDuplicateIndividual.Gender):
+                    return dup.Gender;
                 case nameof(IDisplayDuplicateIndividual.MatchIndividualID):
-                    return ind.MatchIndividualID;
+                    return dup.MatchIndividualID;
                 case nameof(IDisplayDuplicateIndividual.MatchName):
-                    return ind.MatchName;
+                    return dup.MatchName;
                 case nameof(IDisplayDuplicateIndividual.MatchBirthDate):
-                    return ind.MatchBirthDate;
+                    return dup.MatchBirthDate;
                 case nameof(IDisplayDuplicateIndividual.MatchBirthLocation):
-                    return ind.MatchBirthLocation;
+                    return dup.MatchBirthLocation;
                 case nameof(IDisplayDuplicateIndividual.Score):
-                    return ind.Score;
+                    return dup.Score;
                 case nameof(IDisplayDuplicateIndividual.IgnoreNonDuplicate):
-                    return ind.IgnoreNonDuplicate;
+                    return dup.IgnoreNonDuplicate;
+                case nameof(IDisplayDuplicateIndividual.MatchGender):
+                    return dup.MatchGender;
             }
             return null;
         }
