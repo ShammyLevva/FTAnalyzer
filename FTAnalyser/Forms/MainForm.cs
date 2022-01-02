cfb590617fb45514cc2d238e1bd0f38c2c750f1d
ShammyLevva <mega2019www@gmail.com>
2022-01-02 15:05:12 +0000

    Update packages

--- FTAnalyser/Forms/MainForm.cs
+++ FTAnalyser/Forms/MainForm.cs
@@ -30,7 +30,7 @@ namespace FTAnalyzer
 {
     public partial class MainForm : Form
     {
-        public static string VERSION = "8.5.0.0";
+        public static string VERSION = "8.5.0.0-beta1";
         static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
         Cursor storedCursor = Cursors.Default;
