a0cf95c81edc168680799ead16deff8a8f715062
ShammyLevva <mega2019www@gmail.com>
2022-07-25 19:07:45 +0100

    Update master to dot net 6

--- FTAnalyzer.Windows/Properties/MappingSettings.Designer.cs
+++ FTAnalyzer.Windows/Properties/MappingSettings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace FTAnalyzer.Properties {
+namespace FTAnalyzer.Windows.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
