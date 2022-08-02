59559dd0b057bfd4cae29e1043c9561e57f9ea09
ShammyLevva <mega2019www@gmail.com>
2022-08-02 20:48:09 +0100

    Remove old .Net Framework DLLs and replace with .Net 6 local nuget package

--- FTAnalyzer.Windows/Forms/MainForm.cs
+++ FTAnalyzer.Windows/Forms/MainForm.cs
@@ -7,23 +7,14 @@ using FTAnalyzer.UserControls;
 using FTAnalyzer.Utilities;
 using Ionic.Zip;
 using Printing.DataGridViewPrint.Tools;
-using System;
-using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Drawing.Printing;
 using System.Drawing.Text;
-using System.IO;
-using System.Linq;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 using System.Xml;
 using HtmlAgilityPack;
-using System.Net;
 using System.Diagnostics;
 
 namespace FTAnalyzer
