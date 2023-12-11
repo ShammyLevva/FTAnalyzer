0677833e4f4f0b4f611516160c235c172a090af6
ShammyLevva <mega2019www@gmail.com>
2023-12-11 19:38:15 +0000

    Add find to SortableBindingList for filter to work

--- FTAnalyzer.Windows/Forms/MainForm.cs
+++ FTAnalyzer.Windows/Forms/MainForm.cs
@@ -23,7 +23,7 @@ namespace FTAnalyzer
 {
     public partial class MainForm : Form
     {
-        public static readonly string VERSION = "10.0.0.0-beta 4";
+        public static readonly string VERSION = "10.0.0.0-beta 5";
         static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
         Cursor storedCursor = Cursors.Default;
