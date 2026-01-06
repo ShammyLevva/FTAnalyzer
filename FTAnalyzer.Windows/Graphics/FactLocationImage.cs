cfc4c97f01c50b92632f6b3a543baf920b5aa32e
ShammyLevva <ambisset@btinternet.com>
2026-01-06 14:18:52 +0000

    Move Graphics back into Windows

---
+++ FTAnalyzer.Windows/Graphics/FactLocationImage.cs
@@ -0,0 +1,50 @@
+﻿namespace FTAnalyzer.Graphics
+{
+    public class FactLocationImage(FactLocation.Geocode errorLevel, Image img)
+    {
+        public FactLocation.Geocode ErrorLevel { get; private set; } = errorLevel;
+        public Bitmap Icon { get; private set; } = GraphicsUtilities.ResizeImageToCurrentScale(img);
+
+        static readonly FactLocationImage IMG_NOT_SEARCHED = new(FactLocation.Geocode.NOT_SEARCHED,
+            Image.FromFile(Path.Combine(Application.StartupPath, @"Resources\Icons\QuestionMark.png")));
+        static readonly FactLocationImage IMG_MATCHED = new(FactLocation.Geocode.MATCHED,
+            Image.FromFile(Path.Combine(Application.StartupPath, @"Resources\Icons\GoogleMatch.png")));
+        static readonly FactLocationImage IMG_PARTIAL_MATCH = new(FactLocation.Geocode.PARTIAL_MATCH,
+            Image.FromFile(Path.Combine(Application.StartupPath, @"Resources\Icons\GooglePartial.png")));
+        static readonly FactLocationImage IMG_GEDCOM_USER = new(FactLocation.Geocode.GEDCOM_USER,
+            Image.FromFile(Path.Combine(Application.StartupPath, @"Resources\Icons\Complete_OK.png")));
+        static readonly FactLocationImage IMG_NO_MATCH = new(FactLocation.Geocode.NO_MATCH,
+            Image.FromFile(Path.Combine(Application.StartupPath, @"Resources\Icons\CriticalError.png")));
+        static readonly FactLocationImage IMG_INCORRECT = new(FactLocation.Geocode.INCORRECT,
+            Image.FromFile(Path.Combine(Application.StartupPath, @"Resources\Icons\Flagged.png")));
+        static readonly FactLocationImage IMG_OUT_OF_BOUNDS = new(FactLocation.Geocode.OUT_OF_BOUNDS,
+            Image.FromFile(Path.Combine(Application.StartupPath, @"Resources\Icons\OutOfBounds.png")));
+        static readonly FactLocationImage IMG_LEVEL_MISMATCH = new(FactLocation.Geocode.LEVEL_MISMATCH,
+            Image.FromFile(Path.Combine(Application.StartupPath, @"Resources\Icons\Warning.png")));
+        static readonly FactLocationImage IMG_OS50k_MATCH = new(FactLocation.Geocode.OS_50KMATCH,
+            Image.FromFile(Path.Combine(Application.StartupPath, @"Resources\Icons\OS50kMatch.png")));
+        static readonly FactLocationImage IMG_OS50k_PARTIAL = new(FactLocation.Geocode.OS_50KPARTIAL,
+            Image.FromFile(Path.Combine(Application.StartupPath, @"Resources\Icons\OS50kPartial.png")));
+        static readonly FactLocationImage IMG_OS50k_FUZZY = new(FactLocation.Geocode.OS_50KFUZZY,
+            Image.FromFile(Path.Combine(Application.StartupPath, @"Resources\Icons\OS50kFuzzy.png")));
+
+        public static FactLocationImage ErrorIcon(FactLocation.Geocode errorLevel)
+        {
+            return errorLevel switch
+            {
+                FactLocation.Geocode.NOT_SEARCHED => IMG_NOT_SEARCHED,
+                FactLocation.Geocode.MATCHED => IMG_MATCHED,
+                FactLocation.Geocode.PARTIAL_MATCH => IMG_PARTIAL_MATCH,
+                FactLocation.Geocode.GEDCOM_USER => IMG_GEDCOM_USER,
+                FactLocation.Geocode.NO_MATCH => IMG_NO_MATCH,
+                FactLocation.Geocode.INCORRECT => IMG_INCORRECT,
+                FactLocation.Geocode.OUT_OF_BOUNDS => IMG_OUT_OF_BOUNDS,
+                FactLocation.Geocode.LEVEL_MISMATCH => IMG_LEVEL_MISMATCH,
+                FactLocation.Geocode.OS_50KMATCH => IMG_OS50k_MATCH,
+                FactLocation.Geocode.OS_50KPARTIAL => IMG_OS50k_PARTIAL,
+                FactLocation.Geocode.OS_50KFUZZY => IMG_OS50k_FUZZY,
+                _ => IMG_NO_MATCH,
+            };
+        }
+    }
+}
---
+++ FTAnalyzer.Windows/Graphics/GraphicsUtilities.cs
@@ -0,0 +1,58 @@
+﻿using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+namespace FTAnalyzer.Graphics
+{
+    public static class GraphicsUtilities
+    {
+        /// <summary>
+        /// Resize the image to the specified width and height.
+        /// </summary>
+        /// <param name="image">The image to resize.</param>
+        /// <param name="width">The width to resize to.</param>
+        /// <param name="height">The height to resize to.</param>
+        /// <returns>The resized image.</returns>
+        public static Bitmap ResizeImage(Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = System.Drawing.Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using var wrapMode = new ImageAttributes();
+                wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+            }
+
+            return destImage;
+        }
+
+        public static Bitmap ResizeImage(Image image, decimal percentage)
+        {
+            int width = (int)(image.Width * percentage);
+            int height = (int)(image.Height * percentage);
+            return ResizeImage(image, width, height);
+        }
+
+        public static Bitmap ResizeImageToCurrentScale(Image image) => ResizeImage(image, (decimal)GetCurrentScaling());
+
+        public static float GetCurrentScaling()
+        {
+            float dx;
+            using (System.Drawing.Graphics g = Application.OpenForms[0].CreateGraphics())
+            {
+                dx = g.DpiX;
+            }
+            return dx / 96;
+        }
+
+    }
+}
