fc74f07ebdfef691014103628afbb16704711d2b
ShammyLevva <ambisset@btinternet.com>
2026-01-23 12:38:01 +0000

    Remove highDPI manual handling no longer required in .Net10

--- FTAnalyzer.Windows/Program.cs
+++ FTAnalyzer.Windows/Program.cs
@@ -12,19 +12,6 @@ namespace FTAnalyzer.Windows
         public static readonly GoogleClient GoogleClient = new();
 
         /// <summary>
-        /// Enable high DPI
-        /// </summary>
-        static readonly bool HighDPIEnabled = true;
-
-        /// <summary>
-        /// Load for high DPI
-        /// </summary>
-        /// <returns></returns>
-        [LibraryImport("user32.dll")]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        private static partial bool SetProcessDPIAware();
-
-        /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
@@ -33,15 +20,11 @@ namespace FTAnalyzer.Windows
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            if (Environment.OSVersion.Version.Major >= 10)
-                SetProcessDPIAware();
             SharpMapUtility.Configure();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
             MainForm formMain = new();
-            if (HighDPIEnabled)
-                formMain.AutoScaleMode = AutoScaleMode.Dpi;
             Application.Run(formMain);
         }
     }
