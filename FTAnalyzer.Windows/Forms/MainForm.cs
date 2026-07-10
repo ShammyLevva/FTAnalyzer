d9c598697ace276319d9b880a853835dc2832a5f
ShammyLevva <ambisset@btinternet.com>
2026-07-10 12:08:47 +0100

    bump to beta 3

--- FTAnalyzer.Windows/Forms/MainForm.cs
+++ FTAnalyzer.Windows/Forms/MainForm.cs
@@ -24,7 +24,7 @@ namespace FTAnalyzer
 {
     public partial class MainForm : Form
     {
-        public static readonly string VERSION = "11.0.0.0-beta2";
+        public static readonly string VERSION = "11.0.0.0-beta3";
         static readonly log4net.ILog log = log4net.LogManager.GetLogger(typeof(MainForm));
         const string APPNAME = "FTAnalyzer";
 
